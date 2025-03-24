6e85c7b440115abf26a596eedf7442450c448161
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-03-23 21:21:56 -0500

    Ajuste de la vista del listado de comunas

---
+++ app/Models/Municipio.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Cache\HasCacheLock;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Municipio extends Model
+{
+    use HasFactory;
+    protected $table = "tb_municipio";
+    protected $primaryKey = "muni_codi";
+    public $timestamps = false;
+    
+}
