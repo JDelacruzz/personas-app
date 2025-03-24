ae4b72443405b2312ecf1b3e02f1b50f80864da2
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-03-23 21:11:01 -0500

    Configuracion del web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ComunaController;
+/*
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/people/{name}', function ($name) {
     return view('people');
 });
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/comunas', [ComunaController::class, 'index']);
