0017c506218eaaa77d65d0bef8a3c6649413604c
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-03-23 22:59:08 -0500

    Guia terminada

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComunaController;
 /*
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/people/{name}', function ($name) {
     return view('people');
 });
